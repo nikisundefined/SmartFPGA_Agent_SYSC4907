9d4d57f3a94b41d346afbf436ce7f36fab4aad3b
Aakanksh Raj <aakankshraj@cmail.carleton.ca>
2025-03-26 16:17:38 -0400

    Update FPGA Driver code to latest version

--- nengopy/fpga.py
+++ nengopy/fpga.py
@@ -1,78 +1,183 @@
 import struct
 import os.path
+import multiprocessing
+from typing import Optional
 
 import numpy as np
 from pynq import DefaultIP
 from pynq import Overlay
 from pynq import allocate
+from pynq import PL
 from pynq.buffer import PynqBuffer
 from pynq.lib.dma import DMA
+
+# Notes:
+#   DMA channel seems to fail if any transfer fails 
+#       (either due to a lack of output space or insufficient inputs) 
+#       and cannot be restarted without fully restarting the board
+#   If the DMA channel triggers ANY interrupt that is not handled by the system,
+#       the channel locks up until the system is reset (Any interrupt specified under dma_rw.register_map)
+#   IP MUST!! generate TLAST signal otherwise DMA and Pynq have no way of knowing when the IP is finished, 
+#       results is buffers stalling while waiting for something to happen
+#   Writes to the IP that are not written in the register_map but are written in the synthesis are valid, 
+#       however there is not read access to the field so the value must be tracked internally
+#   IP seems to stall when directly writing to the output packet without first copying the return value to another location
+#   Arbitrary sizes of input and output packets can be used however,
+#       since the size cannot be changed, the appropriately sized packet must also be returned
 
 class RectifiedLinearFPGA:
     RectifiedLinear_GainBias = 0
     RectifiedLinear_MaxRateIntercepts = 1
     RectifiedLinear_Step = 2
 
-    def __init__(self, amplitude: float, hls_ip: 'FPGADriver', ol: Overlay):
+    lock = multiprocessing.RLock()
+
+    def __init__(self, amplitude: float, hls_ip: 'FPGADriver', dma: DMA):
         self.amplitude = amplitude
         self.ip = hls_ip
-        self.dma_rw: DMA = ol.ReadWriteDMA
-        self.dma_r0: DMA = ol.ReadDMA0
-        self.dma_r1: DMA = ol.ReadDMA1
+        self.dma = dma
+        self.buffer: PynqBuffer = None
 
-    def step(self, dt: float, J: np.ndarray, output: np.ndarray) -> None:
-        #        output[...] = self.amplitude * np.maximum(0.0, J) # Original Implementation
-        fpga_output = allocate(shape=(output.shape[0], 4), dtype=np.float64)
-        fpga_J = allocate(shape=J.shape, dtype=np.float64)
+    def _allocate(self, elems) -> None:
+        with RectifiedLinearFPGA.lock:
+            if self.buffer is not None:
+                del self.buffer
+            self.buffer = allocate(shape=(elems, 4), dtype=np.float64)
+    
+    def _load_buffer(self, function: int, arg0: np.ndarray, arg1: Optional[np.ndarray] = None, arg2: Optional[np.ndarray] = None) -> None:
+        # Ensure there is enough space in the buffer
+        if self.buffer is None or arg0.size < self.buffer.shape[0]:
+            self._allocate(arg0.size)
+        # Copy elements into the buffer
+        self.buffer[:, 0] = function
+        self.buffer[:, 1] = arg0.ravel()
+        # Only put arg1 and arg2 if they were specified
+        if arg1 is not None:
+            self.buffer[:, 2] = arg1.ravel()
+        if arg2 is not None:
+            self.buffer[:, 3] = arg2.ravel()
 
-        # Load inputs to computation
-        fpga_J[:] = J
-
-        # Setup Internal IP registers
-        self.ip.write(FPGADriver.Function_Select, RectifiedLinearFPGA.RectifiedLinear_Step)
-        self.ip.write(FPGADriver.Amplitude_Low, struct.pack('d', self.amplitude))
-
-        # Send inputs to FPGA
-        self.dma_rw.recvchannel.transfer(fpga_output)
-        self.dma_r0.sendchannel.transfer(fpga_J)
-
-        # Start the IP
+    def _run(self) -> None:
+        # Transfer the buffer address to the DMA channels
+        self.dma.recvchannel.transfer(self.buffer)
+        self.dma.sendchannel.transfer(self.buffer)
+        # Start the accelerator
         self.ip.write(0x0, 0x1)
 
-        # Copy output from FPGA buffer to return buffer
-        output[:] = fpga_output[..., 0]
+    def gain_bias(self, max_rates: np.ndarray, intercepts: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Compute the gain and bias needed to satisfy max_rates, intercepts.
 
-    def max_rates_intercepts(self, gain: np.ndarray, bias: np.ndarray) -> tuple:
-        fpga_gain : PynqBuffer = allocate(shape=gain.shape, dtype=np.float64)
-        fpga_bias: PynqBuffer = allocate(shape=gain.shape, dtype=np.float64)
-        fpga_output: PynqBuffer = allocate(shape=(gain.shape[0], 4), dtype=np.float64)
+        This takes the neurons, approximates their response function, and then
+        uses that approximation to find the gain and bias value that will give
+        the requested intercepts and max_rates.
 
-        fpga_gain[:] = gain
-        fpga_bias[:] = bias
+        Determine gain and bias by shifting and scaling the lines.
 
-        self.ip.write(0x0, 0x01)
-        self.ip.write(FPGADriver.Function_Select, RectifiedLinearFPGA.RectifiedLinear_GainBias)
+        Parameters
+        ----------
+        max_rates : (n_neurons,) array_like
+            Maximum firing rates of neurons.
+        intercepts : (n_neurons,) array_like
+            X-intercepts of neurons.
 
-        self.dma_rw.recvchannel.transfer(fpga_output)
-        self.dma_r1.sendchannel.transfer(fpga_bias)
-        self.dma_r0.sendchannel.transfer(fpga_gain)
+        Returns
+        -------
+        gain : (n_neurons,) array_like
+            Gain associated with each neuron. Sometimes denoted alpha.
+        bias : (n_neurons,) array_like
+            Bias current associated with each neuron.
+        """
+        assert max_rates.shape == intercepts.shape, f"RectifiedLinearFPGA::gain_bias -> max_rates and intercepts must have the same shape | {max_rates.shape} != {intercepts.shape}"
+        # Obtain views on the input arguments in with the corrent datatype
+        max_rates = np.array(max_rates, ndmin=1, copy=False, dtype=np.float64)
+        intercepts = np.array(intercepts, ndmin=1, copy=False, dtype=np.float64)
 
-        return fpga_output[:, 0], fpga_output[:, 1]
+        with RectifiedLinearFPGA.lock: # Aquire the lock on the FPGA just incase
+            # Load buffer with the arguments in the required structure
+            self._load_buffer(RectifiedLinearFPGA.RectifiedLinear_GainBias, max_rates, intercepts)
+
+            # Compute the result
+            self._run()
+
+            # Copy the results out of the buffer
+            gain = np.array(self.buffer[:, 1], ndmin=1, copy=True, dtype=np.float64)
+            bias = np.array(self.buffer[:, 2], ndmin=1, copy=True, dtype=np.float64)
+        return gain, bias
+    
+    def max_rates_intercepts(self, gain: np.ndarray, bias: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Compute the max_rates and intercepts given gain and bias.
+
+        Compute the inverse of gain_bias.
+
+        Parameters
+        ----------
+        gain : (n_neurons,) array_like
+            Gain associated with each neuron. Sometimes denoted alpha.
+        bias : (n_neurons,) array_like
+            Bias current associated with each neuron.
+
+        Returns
+        -------
+        max_rates : (n_neurons,) array_like
+            Maximum firing rates of neurons.
+        intercepts : (n_neurons,) array_like
+            X-intercepts of neurons.
+        """
+        assert gain.shape == bias.shape, f"RectifiedLinearFPGA::max_rates_intercepts -> gain and bias must have the same shape | {gain.shape} != {bias.shape}"
+        
+        gain = np.array(gain, ndmin=1, copy=False, dtype=np.float64)
+        bias = np.array(bias, ndmin=1, copy=False, dtype=np.float64)
+
+        with RectifiedLinearFPGA.lock:
+            self._load_buffer(RectifiedLinearFPGA.RectifiedLinear_MaxRateIntercepts, gain, bias)
+
+            self._run()
+
+            max_rates = np.array(self.buffer[:, 1], ndmin=1, copy=True, dtype=np.float64)
+            intercepts = np.array(self.buffer[:, 2], ndmin=1, copy=True, dtype=np.float64)
+        return max_rates, intercepts
+    
+    def step(self, dt, J, output, *, amplitude: float | None = None) -> None:
+        """
+        Implements the differential equation for this neuron type.
+        
+        Implement the rectification nonlinearity.
+
+        Parameters
+        ----------
+        dt : float
+            Simulation timestep.
+        J : (n_neurons,) array_like
+            Input currents associated with each neuron.
+        output : (n_neurons,) array_like
+            Output activity associated with each neuron (e.g., spikes or firing rates).
+        state : {str: array_like}
+            State variables associated with the population.
+        """
+        J = np.array(J, ndmin=1, copy=False, dtype=np.float64)
+
+        with RectifiedLinearFPGA.lock:
+            self._load_buffer(RectifiedLinearFPGA.RectifiedLinear_Step, amplitude, J)
+
+            self._run()
+
+            output[...] = self.buffer[:, 1]
+        
 
 class FPGADriver(DefaultIP):
-    Function_Select = 0x10
-    Amplitude_Low = 0x18
-    Amplitude_High = 0x1c
 
     def __init__(self, description):
         super().__init__(description)
 
     bindto = ['xilinx.com:hls:nengofpga:1.0']
 
-    def rectified_linear(self, ol: Overlay) -> RectifiedLinearFPGA:
-        return RectifiedLinearFPGA(1, self, ol)
+    def rectified_linear(self, dma: DMA) -> RectifiedLinearFPGA:
+        return RectifiedLinearFPGA(1, self, dma)
 
 if __name__ == '__main__':
+    PL.reset() # Reset any cached versions of the bitstream and hardware info
     bitstream_path: str = '/home/xilinx/nengofpga/nengofpga.bit'
     print("Programming the FPGA")
     print("Path to bitstream")
@@ -85,64 +190,25 @@ if __name__ == '__main__':
     ol = Overlay(bitstream_path)
     print("FPGA programmed")
 
-    if 'nengofpga_0' not in ol.ip_dict.keys():
-        raise RuntimeError('NengoFPGA not found')
+    if 'nengofpga_0' not in ol.ip_dict.keys() or 'ReadWriteDMA' not in ol.ip_dict.keys():
+        raise RuntimeError('NengoFPGA or DMA not found')
     hls_ip: FPGADriver = ol.nengofpga_0
-    neuron: RectifiedLinearFPGA = hls_ip.rectified_linear(ol)
-
-# Old Script Testing Code
-'''
-print("Programming the FPGA")
-ol = Overlay('/home/xilinx/nengofpga/nengofpga.bit')
-
-print("Inspect all the IP names")
-print(ol.ip_dict.keys())
-
-print("Inspect the HLS IP registers")
-hls_ip = ol.step_0
-print(hls_ip)
-
-dma = ol.axi_dma_0
-dma_send = ol.axi_dma_0.sendchannel
-dma_recv = ol.axi_dma_0.recvchannel
-
-print("Starting HLS IP")
-print(hls_ip.register_map)
-CONTROL_REGISTER = 0x0
-hls_ip.write(CONTROL_REGISTER, 0x81)
+    dma_rw: DMA = ol.ReadWriteDMA
+    neuron: RectifiedLinearFPGA = hls_ip.rectified_linear(dma_rw)
 
 
-data_size = 5
-input_buffer = allocate(shape=(data_size,), dtype=np.float64)
-output_buffer = allocate(shape=(data_size,), dtype=np.float64)
-
-a = [i for i in range(data_size)]
-a = np.float64(a)
-#for i in range(data_size):
-#    input_buffer[i] = np.float32(i)
-np.copyto(input_buffer, a)
-
-print("Starting data transfer")
-dma_recv.transfer(output_buffer)
-dma_send.transfer(input_buffer)
-
-print("Buffers")
-print(input_buffer)
-print(output_buffer)
-
-print("State")
-print(dma_send.idle)
-print(dma_recv.idle)
-
-#print("Waiting for transfer")
-#dma_send.wait()
-#dma_recv.wait()
-
-#for i in range(data_size):
-#    print(i, input_buffer[i], output_buffer[i])
-
-#del input_buffer
-#del output_buffer
-#del ol
-#print("End of code")
-'''
+# PL.reset() # Reset any cached versions of the bitstream and hardware info
+# bitstream_path: str = '/home/xilinx/nengofpga/nengofpga.bit'
+# ol = Overlay(bitstream_path)
+# ol.ip_dict.keys()
+# hls_ip: DefaultIP = ol.nengofpga_0
+# hls_ip.register_map
+# dma_rw: DMA = ol.ReadWriteDMA
+# dma_rw.register_map
+# fpga_input: PynqBuffer = allocate(shape=(4,4), dtype=np.float64)
+# fpga_output: PynqBuffer = allocate(shape=(4,4), dtype=np.float64)
+# fpga_input[:, 1:-1] = np.random.rand(4, 2)
+# fpga_input
+# dma_rw.recvchannel.transfer(fpga_output)
+# dma_rw.sendchannel.transfer(fpga_input)
+# fpga_output
